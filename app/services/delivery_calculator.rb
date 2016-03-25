76a939a4820ee22778464075ad35ad2387d34561
Logan Frederick <loganfrederick@gmail.com>
2016-03-25 13:33:01 -0500

    Temp

--- app/services/delivery_calculator.rb
+++ app/services/delivery_calculator.rb
@@ -6,7 +6,7 @@ class DeliveryCalculator
   end
 
   def cost_of_deliveries_within_date_range(start_date, end_date)
-    placement = Placement.find_by(placement_id: placement_id)
+    placement = Placement.where(placement_id: placement_id).where()
     impressions = Delivery.where(placement_id: placement_id).where("date >= '#{start_date}' AND date <= '#{end_date}'").pluck(:impressions)
     placement_cost = (total_impressions(impressions) / 1000) * placement.cpm
   end
